887ac00eccbc6be988b553b2215fcf75de36d6a0
lovro <lovro.zagar5@gmail.com>
2023-02-02 20:30:58 +0000

    build: first commit, add files

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+import formValidation from './modules/formValidation'
 import './styles/main.scss'
+import './assets/bg.png'
+import './assets/bg2.svg'
+
+formValidation.domOnLoad()
